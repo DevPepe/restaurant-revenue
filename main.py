fef744406aba6f85d4c0abfff86c1efe212d5549
DevPepe <devpepe86@gmail.com>
2015-03-30 23:36:33 +0100

    More get Methods

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from distribution import Distribution
 
 if __name__ == "__main__":
     d = Data("/Users/Navarro/Documents/Kaggle_Competition/Restaurant_Revenue_Prediction/restaurant-revenue/Data/train.csv")
-    d.getOpenDate('old')
+    l = d.getP(37)
+    print len(l)
