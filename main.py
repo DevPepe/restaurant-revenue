518fe2480bf647e9587bff8cfba39b916271c714
DevPepe <devpepe86@gmail.com>
2015-03-30 01:12:49 +0100

    Minor changes

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from data import Data
 from distribution import Distribution
 
 if __name__ == "__main__":
-    d = Data("/Users/Navarro/Documents/Kaggle_Competition/Restaurant_Revenue_Prediction/restaurant-revenue/Data/test.csv")
+    d = Data("/Users/Navarro/Documents/Kaggle_Competition/Restaurant_Revenue_Prediction/restaurant-revenue/Data/train.csv")
     d.getOpenDate('old')
